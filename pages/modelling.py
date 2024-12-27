5b5d338cae69ade1d84a728c753cd066ec7d1bb0
ashl3y-ds <ashleycyc-wp21@student.tarc.edu.my>
2024-12-27 14:07:21 +0800

    Update modelling.py

--- pages/modelling.py
+++ pages/modelling.py
@@ -11,9 +11,9 @@ if "filtered_df" in st.session_state:
     st.write(st.session_state["ranked_data"].head())
 
       # Split data
-        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
         # Scale data
         scaler = StandardScaler()
-        x_train = scaler.fit_transform(X_train)
-        x_test = scaler.transform(X_test)
+        X_train = scaler.fit_transform(X_train)
+        X_test = scaler.transform(X_test)
