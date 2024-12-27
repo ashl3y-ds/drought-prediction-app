44ab8d2530e691d808e75dc06e982f4773182ef6
ashl3y-ds <ashleycyc-wp21@student.tarc.edu.my>
2024-12-27 13:59:46 +0800

    Update modelling.py

--- pages/modelling.py
+++ pages/modelling.py
@@ -1,15 +1,9 @@
 # For re-use of ranked data in other parts of the app
-if "ranked_data" in st.session_state:
+if "filtered_df" in st.session_state:
     st.write("### Ranked Data Preview:")
     st.write(st.session_state["ranked_data"].head())
 
-    # Example: Splitting data again later using ranked data
-    if st.button("Split and Save Ranked Data for Training"):
-        ranked_df = st.session_state["ranked_data"]
-        X = ranked_df.drop(columns=["target"])
-        y = ranked_df["target"]
-
-        # Split data
+      # Split data
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
         # Scale data
