022ae41610865e606c8e0e11c514b6fb35ce4c4e
ashl3y-ds <ashleycyc-wp21@student.tarc.edu.my>
2024-12-27 22:56:48 +0800

    Update view_data.py

--- pages/view_data.py
+++ pages/view_data.py
@@ -1,9 +1,8 @@
 import streamlit as st
 
-st.title("View Data")
-st.write("This page displays the combined dataset.")
+st.write("Drought Prediction")
 
-if "combined_df" in st.session_state:
+if "cleaned_df" in st.session_state:
     st.dataframe(st.session_state["combined_df"])
 else:
     st.warning("No dataset found! Please upload and combine data on the Upload Data page.")
