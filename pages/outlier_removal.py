0b3b37cd1937f914c0b559c0438a9c585b078a47
ashl3y-ds <ashleycyc-wp21@student.tarc.edu.my>
2024-12-27 12:41:02 +0800

    Update outlier_removal.py

--- pages/outlier_removal.py
+++ pages/outlier_removal.py
@@ -98,6 +98,8 @@ else:
     # Create a heatmap plot of the correlation matrix
     st.write("### Correlation Heatmap")
 
+        plt.figure(figsize=(12, 8))
+
     sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f", linewidths=0.5, cbar_kws={"shrink": .8})
 
     # Set title and labels
