dfb051100a002c46336b783cc9aff3d49cc2261c
ashl3y-ds <ashleycyc-wp21@student.tarc.edu.my>
2024-12-27 15:53:01 +0800

    Update modelling.py

--- pages/modelling.py
+++ pages/modelling.py
@@ -71,7 +71,7 @@ st.write("### Customized Confusion Matrix with Outside Coloring:")
 fig, ax = plt.subplots(figsize=(8, 6))  # Adjust figure size as needed
 
 # Set custom background colors for the figure and axes
-fig.patch.set_facecolor("#363837")  # Background color for the figure
+fig.patch.set_facecolor("#e0e0e0")  # Background color for the figure
 ax.set_facecolor("#363837")         # Background color for the axes
 
 # Plot the confusion matrix as integers
