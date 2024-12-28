3d14b62a4b27ebf2fb8f0da8c463ab552ea1360a
ashl3y-ds <ashleycyc-wp21@student.tarc.edu.my>
2024-12-29 06:35:35 +0800

    Update Data_Visualization.py

--- pages/Data_Visualization.py
+++ pages/Data_Visualization.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+from matplotlib.colors import LinearSegmentedColormap
 
 @st.cache_data
 def load_data():
